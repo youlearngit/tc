6e7dad965411ccb416d9914336f2eacdfcf7547f
youlearngit <2652908830@qq.com>
2018-10-16 08:57:21 +0800

    更新

---
+++ tc/data/caches/adv_other.php
@@ -0,0 +1,18 @@
+<?php
+$data = array (
+  'all' => 
+  array (
+    'type' => 
+    array (
+      'intercatad' => 
+      array (
+        0 => '22',
+      ),
+    ),
+  ),
+  'items' => 
+  array (
+    22 => '<a href="#" target="_blank"><img src="/attachment/ggg/160x600.gif" border="0"></a>',
+  ),
+);
+?>
